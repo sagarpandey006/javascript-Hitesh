13fbdc8f04bd3cf7af1791fc9c50611335516a3f
Sagar Pandey <151049831+sagarpandey006@users.noreply.github.com>
2025-01-18 18:04:58 +0000

    arrays

--- 02_basic/02_arrays.js
+++ 02_basic/02_arrays.js
@@ -13,14 +13,14 @@ const all_new_heros = [...marvel_heros, ...dc_heros]
 
 // console.log(all_new_heros);
 
-const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array);
 
 
 
-console.log(Array.isArray("sagar"))
+// console.log(Array.isArray("sagar"))
 console.log(Array.from("sagar"))
 console.log(Array.from({name: "sagar"})) // interesting
 
