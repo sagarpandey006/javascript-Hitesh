4534fdbcd5b43b901b44f9e8e6fe66d0a17d6c22
Sagar Pandey <151049831+sagarpandey006@users.noreply.github.com>
2025-01-31 10:05:33 +0000

    objects

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -28,3 +28,54 @@ const obj4 = {5: "a", 6: "b"}
 
 const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
