a00717dc2b48db36531e68e532a65afb807ab8ea
Sagar Pandey <151049831+sagarpandey006@users.noreply.github.com>
2025-03-08 15:50:12 +0000

    promises inside promise

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -10,3 +10,23 @@ const promiseOne = new Promise(function(resolve, reject){
 promiseOne.then(function(){
     console.log("Promise consumed");
 })
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+    }, 1000)
+
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "Chai", email: "chai@example.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
